c43551573ee570678bd4516783848aa1ef65dddd
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-10 12:07:45 -0600

    push before merge

--- providers.py
+++ providers.py
@@ -5,7 +5,7 @@ import json
 
 # python -c 'import providers; providers.getProviders()'
 def getProviders():
-    resp = requests.get("http://localhost:3000/api/v1/providers/")
+    resp = requests.get("http://api.simplymailstatistics.com/api/v1/providers/")
     providers = []
     if resp.status_code != 200:
         raise ApiError('GET /providers {}'.format(resp.status_code))
@@ -16,7 +16,7 @@ def getProviders():
 # python -c 'import providers; providers.getProvidersByUserId(18)'
 def getProvidersByUserId(id):
     userProviders = []
-    resp = requests.get("http://localhost:3000/api/v1/providers/%s" % id)
+    resp = requests.get("https://api.simplymailstatistics.com/api/v1/providers/%s" % id)
     if resp.status_code != 200:
         raise ApiError('GET /providers/:id {}'.format(resp.status_code))
     for provider in resp.json():
@@ -25,7 +25,7 @@ def getProvidersByUserId(id):
 
 # python -c 'import providers; providers.getProvidersByUserId(18)'
 def getProviderNames(id):
-    resp = requests.get("http://localhost:3000/api/v1/providers-names/%s" % id)
+    resp = requests.get("http://api.simplymailstatistics.com/api/v1/providers-names/%s" % id)
     if resp.status_code != 200:
         raise ApiError('GET /providers-names/:id {}'.format(resp.status_code))
     for provider in resp.json():
--- sumEvents.py
+++ sumEvents.py
@@ -17,12 +17,14 @@ def sumOpens(data):
         opens.append(provider.opens)
     return 'Opens: ' + str(sum(opens))
 
+# python -c 'import sumEvents; sumEvents.sumSpamReports(18)'
 def sumSpamReports(data):
     spam_reports = []
     for provider in data:
         spam_reports.append(provider.spam_reports)
     return 'Spam Reports: ' + str(sum(spam_reports))
 
+# python -c 'import sumEvents; sumEvents.returnEvents(18)'
 def returnEvents(id):
     data = getProviders(id)
     print sumDelivered(data)
