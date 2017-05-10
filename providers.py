0b1450b695a89d12a7a13914b7940c8ad7bc6d6b
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-09 18:23:09 -0600

    returns provider names

--- providers.py
+++ providers.py
@@ -17,5 +17,10 @@ def getProvidersByUserId(id):
     for provider in resp.json():
         print('{} {}'.format(provider['id'], provider['provider_name']))
 
-def get ProviderNames():
-    resp = requests.get("http://localhost:3000/api/v1/providers/")
+# python -c 'import providers; providers.getProvidersByUserId(18)'
+def getProviderNames(id):
+    resp = requests.get("http://localhost:3000/api/v1/providers-names/%s" % id)
+    if resp.status_code != 200:
+        raise ApiError('GET /providers-names/:id {}'.format(resp.status_code))
+    for provider in resp.json():
+        print('{}'.format(provider))
