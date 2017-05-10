5b7f54235c9b7c7b885f8ad271e09173ae94edb0
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-10 10:28:00 -0600

    parsing data

--- providers.py
+++ providers.py
@@ -1,25 +1,27 @@
 from provider import *
 import requests
-import pdb
 import json
 # pdb.set_trace()
 
 # python -c 'import providers; providers.getProviders()'
 def getProviders():
     resp = requests.get("http://localhost:3000/api/v1/providers/")
+    providers = []
     if resp.status_code != 200:
         raise ApiError('GET /providers {}'.format(resp.status_code))
     for provider in resp.json():
         p = Provider(**provider)
-        print p
+        print p.provider_name
 
 # python -c 'import providers; providers.getProvidersByUserId(18)'
 def getProvidersByUserId(id):
+    userProviders = []
     resp = requests.get("http://localhost:3000/api/v1/providers/%s" % id)
     if resp.status_code != 200:
         raise ApiError('GET /providers/:id {}'.format(resp.status_code))
     for provider in resp.json():
-        print('{} {}'.format(provider['id'], provider['provider_name']))
+        userProviders.append(Provider(**provider))
+        return userProviders
 
 # python -c 'import providers; providers.getProvidersByUserId(18)'
 def getProviderNames(id):
---
+++ sumEvents.py
@@ -0,0 +1,6 @@
+import providers
+
+def sumDelivered(id):
+    import pdb; pdb.set_trace()
+    data = providers.getProvidersByUserId(id)
+    print data
