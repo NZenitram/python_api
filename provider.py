2b5562c8d7ce586904da3bb0d2b346b9e9bad44c
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-10 09:27:13 -0600

    created some objects

---
+++ provider.py
@@ -0,0 +1,3 @@
+class Provider(object):
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
--- providers.py
+++ providers.py
@@ -1,13 +1,17 @@
+from provider import *
 import requests
 import pdb
+import json
+# pdb.set_trace()
 
 # python -c 'import providers; providers.getProviders()'
 def getProviders():
     resp = requests.get("http://localhost:3000/api/v1/providers/")
     if resp.status_code != 200:
-        raise ApiError('GET /providers/:id {}'.format(resp.status_code))
+        raise ApiError('GET /providers {}'.format(resp.status_code))
     for provider in resp.json():
-        print('{} {}'.format(provider['id'], provider['provider_name']))
+        p = Provider(**provider)
+        print p
 
 # python -c 'import providers; providers.getProvidersByUserId(18)'
 def getProvidersByUserId(id):
