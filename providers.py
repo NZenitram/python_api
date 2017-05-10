bf73c61c07c02e71a06141e31ab79f8d42c22e56
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-05-10 14:36:52 -0600

    added list comprehension for Providers by user id

--- providers.py
+++ providers.py
@@ -5,13 +5,13 @@ import json
 # python -c 'import providers; providers.getProviders()'
 def getProviders():
     # resp = requests.get("https://api.simplymailstatistics.com/api/v1/providers/")
-    resp = requests.get("http://localhost:3000.com/api/v1/providers")
+    resp = requests.get("http://localhost:3000/api/v1/providers")
     providers = []
     if resp.status_code != 200:
         raise ApiError('GET /providers {}'.format(resp.status_code))
     for provider in resp.json():
-        p = Provider(**provider)
-        print p.provider_name
+        providers.append(Provider(**provider))
+    return providers
 
 # python -c 'import providers; providers.getProvidersByUserId(18)'
 def getProvidersByUserId(id):
@@ -20,8 +20,7 @@ def getProvidersByUserId(id):
     resp = requests.get("http://localhost:3000/api/v1/providers/%s" % id)
     if resp.status_code != 200:
         raise ApiError('GET /providers/:id {}'.format(resp.status_code))
-    for provider in resp.json():
-        userProviders.append(Provider(**provider))
+    userProviders = [Provider(**provider) for provider in resp.json()]
     return userProviders
 
 # python -c 'import providers; providers.getProvidersByUserId(18)'
